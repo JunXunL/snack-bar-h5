19406c07a2738b51e6f8ee9ed7723d2d032d1ed2
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-09-22 14:25:18 +0800

    feat: 轮播图组件

---
+++ src/components/public/swiper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 11 -->
+  </div>
+</template>
+<script>
+// import axios from 'axios'
+// import Vue from 'vue';
+// import { Swipe, SwipeItem } from 'vant';
+export default {
+  //   components: {
+  //     Swipe,
+  //     SwipeItem
+  //   },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+* {
+  display: flex;
+  flex-flow: column;
+}
+</style>
